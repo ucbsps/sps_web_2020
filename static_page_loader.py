50d98188ed49d2430cd278faf480d6c472c80693
Carter Turn <carterturn@berkeley.edu>
2020-08-07 19:05:55 -0700

    Static pages are proper html

--- static_page_loader.py
+++ static_page_loader.py
@@ -1,27 +1,39 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.template.loader import render_to_string, TemplateDoesNotExist
+from django.template.loader import render_to_string
 
+from os import path
 import xml.etree.ElementTree as ET
 
-from settings import CONTENT_DIR
+from settings import BASE_DIR
 from error_handler import error_500
 
 def load_static_page(request, page_name):
     try:
-        content = render_to_string('{}.html'.format(page_name))
-    except TemplateDoesNotExist:
-        return error_500(request, page_name)
+        page_tree = ET.parse(path.join(BASE_DIR, 'sps_web_2020/static_html/{}.html'.format(page_name)))
+    except FileNotFoundError:
+        return error_500(request)
+    except ET.ParseError:
+        return error_500(request)
 
-    data = ET.fromstring('<content>' + content + '</content>')
+    page_tree_root = page_tree.getroot()
+
     title = ''
+    content = ''
 
-    for child in data:
-        if child.tag == 'meta' and 'name' in child.attrib:
-                if child.attrib['name'] == 'sps:title' and 'content' in child.attrib:
-                    title = child.attrib['content']
+    for child in page_tree_root:
+        if child.tag == 'head':
+            for head_child in child:
+                if head_child.tag == 'title':
+                    title = head_child.text
+        if child.tag == 'body':
+            content = ET.tostring(child)
+
+    # remove body tag from content
+    content = content.replace(b'<body>', b'').replace(b'</body>', b'')
+    # remove whitespace in order to compress content
+    content = content.replace(b'\n', b'').replace(b'\t', b'')
                     
-    return render(request, 'root.html', {'title': title, 'content': content})
+    return render(request, 'root.html', {'title': title, 'content': content.decode()})
 
 def load_officers(request):
 
