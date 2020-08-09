cddb4f1130efeb08e19965758943a3f3322c2080
Carter Turn <carterturn@berkeley.edu>
2020-08-09 15:05:13 -0700

    Docstrings for url_date_converter

--- url_date_converter.py
+++ url_date_converter.py
@@ -1,12 +1,23 @@
+"""
+A Django custom path converter for handling ISO formatted dates.
+
+url_date_converter -- path converter class for handling ISO formatted dates as Python 3 datetimes.
+"""
+
 from datetime import date
 
 class url_date_converter:
+    """
+    A Django path converter for handling ISO formatted dates as Python 3 datetimes.
+    """
     regex = '[0-9]{4}-[0-9]{2}-[0-9]{2}'
 
     def to_python(self, value):
+        """Return the datetime from parsing value as an ISO date."""
 
         return date.fromisoformat(value)
 
     def to_url(self, value):
+        """Return the string ISO representation of a datetime."""
 
         return value.isoformat()
