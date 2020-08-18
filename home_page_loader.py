3236e6fbf538763766aaf399fdea50b04ca8dd58
Carter Turn <carterturn@berkeley.edu>
2020-08-17 19:35:38 -0700

    First version of more complex home page

--- home_page_loader.py
+++ home_page_loader.py
@@ -11,10 +11,45 @@ from os import path
 import xml.etree.ElementTree as ET
 
 from settings import BASE_DIR
+from events_page_loader import get_events
 from error_handler import error_500
 
 def load_home(request):
     """Returns homepage."""
 
-    return render(request, 'root.html', {'title': 'UC Berkeley Society of Physics Students',
-                                         'content': 'UC Berkeley Society of Physics Students'})
+    try:
+        content = render_to_string('home.html',
+                                   {'upcoming_events': get_events(True, count=3)})
+    except TemplateDoesNotExist:
+        return error_500(request, 'Home')
+
+    try:
+        page_tree = ET.ElementTree(ET.fromstring(content))
+    except FileNotFoundError:
+        return error_500(request)
+    except ET.ParseError:
+        return error_500(request)
+
+    page_tree_root = page_tree.getroot()
+
+    header_tags = []
+    content = ''
+
+    for child in page_tree_root:
+        if child.tag == 'head':
+            for head_child in child:
+                if not head_child.tag == 'title':
+                    header_tags.append(ET.tostring(head_child).decode())
+        if child.tag == 'body':
+            content = ET.tostring(child).decode()
+
+    header = ''.join(header_tags)
+
+    # remove body tag from content
+    content = content.replace('<body>', '').replace('</body>', '')
+    # remove whitespace in order to compress content
+    content = content.replace('\n', '').replace('\t', '')
+    header = header.replace('\n', '').replace('\t', '')
+
+    return render(request, 'root.html', {'title': 'UC Berkeley SPS',
+                                         'header': header, 'content': content})
