d582b4261d075018fd7dab43cf13dcc15bdecc26
Carter Turnbaugh <carterturn@ocf.berkeley.edu>
2020-08-24 17:08:20 -0700

    Update setup script

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 python -m pip install virtualenv
 
-virtualenv -p python3 venv
+virtualenv -p python3.7 venv
 
 venv/bin/pip install django
 venv/bin/pip install flup6
-venv/bin/pip install mariadb
+venv/bin/pip install pymysql
