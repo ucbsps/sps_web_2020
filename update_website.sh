3a83e6a6632691b712bd4e370d8f341be600310c
TianyueLi18 <61394591+TianyueLi18@users.noreply.github.com>
2020-08-25 15:51:48 -0700

    add deleted ocf files

---
+++ update_website.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Get website code and static files
+cd /home/p/ph/physics/sps_web_2020
+git checkout release
+git pull
+
+# Copy static files to website
+rsync -rc ~/sps_web_2020/static ~/public_html/
+
+# Trigger update of website code
+touch ~/public_html/run.fcgi
+
+# Get latest sync code
+cd /home/p/ph/physics/sps_web_sync
+git checkout release
+git pull
+
+# Run sync scripts
+venv/bin/python3 download_calendar.py
+venv/bin/python3 download_potw.py
