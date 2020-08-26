0fdef3fe078d2acb80315d18adb4f922c5a7e98f
Carter Turn <carterturn@berkeley.edu>
2020-08-26 06:43:59 -0700

    Debug should be false for release

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['sps.berkeley.edu', 'www.sps.berkeley.edu',
                  'ocf.berkeley.edu', 'www.ocf.berkeley.edu',
