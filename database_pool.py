f060d89b0fe1fd08ba6345ed0df69e2baf120faa
Carter Turn <carterturn@berkeley.edu>
2020-09-13 22:54:04 -0700

    Made name more explicit

--- database_pool.py
+++ database_pool.py
@@ -10,6 +10,6 @@ from DBUtils.PooledDB import PooledDB
 
 from secrets import MARIADB_USER, MARIADB_DB, MARIADB_PASSWORD, MARIADB_HOST
 
-pool = PooledDB(creator=pymysql, host=MARIADB_HOST, user=MARIADB_USER,
-                password=MARIADB_PASSWORD, database=MARIADB_DB,
-                autocommit=True, ping=1)
+database_pool = PooledDB(creator=pymysql, host=MARIADB_HOST, user=MARIADB_USER,
+                         password=MARIADB_PASSWORD, database=MARIADB_DB,
+                         autocommit=True, ping=1)
