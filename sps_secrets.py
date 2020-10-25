7014b46b0e41241e1d6e61fbf79fe8228ffb58ff
Carter Turn <carterturn@berkeley.edu>
2020-10-24 19:37:25 -0700

    Delete secrets file

--- sps_secrets.py
+++
@@ -1,8 +0,0 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y*$16bw)c7=rjo-hi+n-$-$n&lj41p#^uejx6&19x73sbp&pz&'
-
-# SECURITY WARNING: do not give other people access to the database
-MARIADB_USER = 'physics'
-MARIADB_DB = 'physics'
-MARIADB_PASSWORD = 'OUWc2b2EqQO43QhVJd1bi64F'
-MARIADB_HOST = 'localhost'
